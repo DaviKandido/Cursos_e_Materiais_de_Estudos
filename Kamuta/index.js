31e2ccf97e8acd54841df53db7f17e0544fef569
DavyKandido <davicandidopucminas@gmail.com>
2024-08-14 21:15:13 -0300

    comit

---
+++ Kamuta/index.js
@@ -0,0 +1,47 @@
+//var - 
+//let - 
+//const - constantes
+
+
+//Strings
+var nome = "Marcos"
+let nome2 = "Diego"
+const nome3 = "João"
+
+console.log(nome)
+console.log(nome2)
+console.log(nome3)
+
+
+// number
+// 1, 2, 3, 4, 5, 5.5, 6.69...
+//boolean 
+// true, false
+//   1 ,  0
+//object
+
+const pessoa = {
+    nome: "Marcos",
+    idade: 25,
+    endereco: "Rua Ant. Figueira",
+    profissao: "dev",
+    salario: 12000,
+    id: 1
+}
+
+console.log(pessoa.id);
+
+//objeto - array
+
+const lista = [1, 2, 3, 4, "coisa", pessoa, nome2]
+
+console.log(lista);
+
+const pessoas = [
+    { nome: "Marcos", idade: 28 },
+    { nome: "Pedro", idade: 25 },
+]
+console.log(pessoas[1]);
+
+//function
+//Symbol
