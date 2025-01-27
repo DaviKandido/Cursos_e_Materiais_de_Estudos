9d2c420fbf69b12aef0e329791f232953bef7f05
DavyKandido <davicandidopucminas@gmail.com>
2025-01-27 15:49:36 -0300

    exclud

--- 2024/Curso_Dev_GiminiAlura/Back-End/test/src/controllers/barberControllers.js
+++
@@ -1,6 +0,0 @@
-import { getAllBarbers } from "../models/barberModels.js"
-
-export async function getAllBarbersController(req, res) {
-    const barbers = await getAllBarbers()
-    res.status(200).send(barbers)
-}
