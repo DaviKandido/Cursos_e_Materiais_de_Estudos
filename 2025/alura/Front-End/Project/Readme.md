be416cc18814b1314526f4fecd51282b229aad6c
DavyKandido <davicandidopucminas@gmail.com>
2025-01-31 09:45:02 -0300

    comit

--- 2025/alura/Front-End/Project/Readme.md
+++ 2025/alura/Front-End/Project/Readme.md
@@ -5,3 +5,10 @@
 - Nesta semana a Alura abriu as portas as pra uma oportunidade preciosíssima, com mais uma de suas imersões
   enriquecedoras, no qual vi uma oportunidade de aproveitar para refoçar minha base em tecnologias front-end
   e claro aprender algo novo com os incríveis professores Fernanda Degolin (Dev front-end na globo), Mayara Cardoso (Dev front-end no itau) e Guilherme Lima (Desenvolvedor de software e instrutor da Alura)
+
+
+Dependência de JsonServer: 
+  - npm i json-server -g
+
+Para subir a pasta da API no jsonServer
+  - json-server --watch api-artists/artists.json --port 8000
--- 2025/alura/Front-End/Project/clone-spotfy/src/app/page.tsx
+++ 2025/alura/Front-End/Project/clone-spotfy/src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Footer from "../components/Footer/Footer";
 import Library from "../components/Library/Library";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Navigator from "../components/Navigator/Navigator";
+import Cards from "@/components/Cards/Cards";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [pesquisa, setPesquisa] = useState('');
+
+
   return (
     <>
 
@@ -13,16 +20,16 @@ export default function Home() {
         </div>
 
         <div className="max-w-[80vw] ml-[320px] mr-[32px] p-4 m-2 bg-[var(--background-card-black)] rounded-[15px]">
-           <Navigator/>
+           <Navigator pesquisa={pesquisa} setPesquisa={setPesquisa}/>
         </div>
 
-          <main className="bg-[var(--background-card-black)] max-w-[80vw] ml-[320px] mr-[32px] px-10 py-5 rounded-[15px]">
+          <main className="bg-[var(--background-card-black)] max-w-[80vw] ml-[320px] mr-[32px] px-10 rounded-[15px]">
 
-            <div>
-              <h2 className="font-[1000] text-[50px] mb-[40px]">Boa tarde</h2>
-              <p className="font-[700] text-[30px] mb-[20px]">Navegar por todas as seções</p>
-            </div>
+
+            <Cards pesquisa={pesquisa}/>
           </main>
+
+
 
         <Footer/>
     </>
